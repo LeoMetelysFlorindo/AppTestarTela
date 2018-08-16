7c959703d1af24b56814c677ed8d0f8de000a02d
Leo Metelys <leo.metelys@gmail.com>
2018-08-16 08:00:57 -0400

    App Testar Tela
    Aplicativo Xamarin / C#.NET desenvolvido em Visual Studio 2015. Mostra as informações do celular como modelo e as dimensões dpi da tela do celular

---
+++ AppTestarTela/AppTestarTela/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Plugin.DeviceInfo;
+
+namespace AppTestarTela
+{
+    [Activity(Label = "Testar Resolução", MainLauncher = true, Icon = "@drawable/icon")]
+
+     public class MainActivity : Activity
+    {
+
+        
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+
+            //Algoritimo a desenvolver: 09/04/2017 14:48h
+            //detectar o tamanho da tela em uso
+
+            var platform = CrossDeviceInfo.Current.Platform;
+            var versao = CrossDeviceInfo.Current.Version;
+            var modelo = CrossDeviceInfo.Current.Model;      
+                   
+            var metrics = Resources.DisplayMetrics;
+            var widthInDp = ConvertPixelsToDp(metrics.WidthPixels);
+            var heightInDp = ConvertPixelsToDp(metrics.HeightPixels);
+
+
+            //remove título desta Activity
+            //Window.RequestFeature(WindowFeatures.NoTitle);
+
+            //setar apropriadamente qual layout apresentar na tela
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.TestarResolucao);
+            var txtTamanho = FindViewById<TextView>(Resource.Id.txtWidth);
+            var txtLargura = FindViewById<TextView>(Resource.Id.txtHeigth);
+            var TxtPlataforma = FindViewById<TextView>(Resource.Id.txtPlataforma);
+            var TxtVersao = FindViewById<TextView>(Resource.Id.txtVersao);
+            var TxtModel = FindViewById<TextView>(Resource.Id.txtModelo);
+
+            txtTamanho.SetTextColor(Android.Graphics.Color.Yellow);
+            txtLargura.SetTextColor(Android.Graphics.Color.Yellow);
+            txtTamanho.Text = "Width do Celular: " + widthInDp.ToString();
+            txtLargura.Text = "Height do Celular: " + heightInDp.ToString();
+            TxtPlataforma.Text = "Plataforma do Celular: " + platform.ToString();
+            TxtVersao.Text = "Versão do Celular: " + versao.ToString();
+            TxtModel.Text = "Modelo do Celular: " + modelo.ToString();
+        }
+
+        private int ConvertPixelsToDp(float pixelValue)
+        {
+            var dp = (int)((pixelValue) / Resources.DisplayMetrics.Density);
+            return dp;
+        }
+
+    }
+}
+
---
+++ AppTestarTela/AppTestarTela/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("AppTestarTela.MainApplication, AppTestarTela, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5ebc4937dcfa6c46c37e6a765ec4e87dd.MainApplication.class, md5ebc4937dcfa6c46c37e6a765ec4e87dd.MainApplication.__md_methods);
+		
+	}
+}
